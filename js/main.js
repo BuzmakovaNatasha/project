06e5b04e90aa0469ac2b8398241815b5e00cb264
BuzmakovaNatasha <buzmakova_natasha@rambler.ru>
2021-12-30 23:33:19 +0500

    lesson 1
    1. Добавила стили.
    2. Добавила значения по умолчанию для аргументов функций.
    3. Из функции renderProduct убрала return, т.к. при одной команде в стрелочной функции он ставится автоматически.
    4. Запятая между продуктами ставилась, т.к. метод map создает новый массив, а его элементы соответственно разделяются запятыми; я решила этот вопрос с помощью join("").

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 1000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150},
+    { id: 1, title: 'Notebook', price: 1000 },
+    { id: 2, title: 'Mouse', price: 100 },
+    { id: 3, title: 'Keyboard', price: 250 },
+    { id: 4, title: 'Gamepad', price: 150 },
 ];
 
-const renderProduct = (title, price, img = '') => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить</button>
-              </div>`;
-};
+const renderProduct = (title = 'Товар', price = 0, img = 'no-photo.jpg') =>
+    `<div class="product-item">
+        <img src="img/${img}" alt=photo>
+        <h3>${title}</h3>        
+        <p>${price} &#8381</p>
+        <button class="by-btn">Добавить</button>
+    </div>`;
 
 const renderCatalog = (list) => {
-  const productList = list.map(good => renderProduct(good.title, good.price));
+    const productList = list.map(good => renderProduct(good.title, good.price)).join("");
     console.log(productList);
-
-  document.querySelector('.products').innerHTML = productList;
+    document.querySelector('.products').innerHTML = productList;
 };
 
 renderCatalog(products);
